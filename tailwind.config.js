982fb4e56f9068923fa59758845ecddd6aa73b99
Christian Cueni <christian.cueni@iterativ.ch>
2023-03-08 21:15:53 +0100

    Add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+    },
+    fontWeight: {
+      thin: '100',
+      extralight: '200',
+      light: '300',
+      normal: '500',
+      bold: '700',
+      extrabold: '800',
+      black: '900',
+    },
     extend: {},
   },
   plugins: [],
